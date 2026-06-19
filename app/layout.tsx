39c65ff6e5e1461eebb591d420597e96d364162c
el_55 <jesrodriguezalv@gmail.com>
2026-06-19 16:47:50 -0700

    verificacion adsense 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <head>
-        <meta name="google-adsense-account" content="ca-pub-1793586690744453"></meta>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1793586690744453"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>
-        <Navbar />
+        {/* Navbar y demás */}
         {children}
       </body>
     </html>
