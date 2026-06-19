54d58144a2d7883eb13305c7e5ab7bcc4a7d18b1
el_55 <jesrodriguezalv@gmail.com>
2026-06-19 16:38:00 -0700

    verificacion adsense otra forma

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Navbar from './components/Navbar'
 
-export const metadata: Metadata = {
-  title: 'Relatos Oscuros',
-  description: 'Terror y suspenso',
-  other: {
-    'google-adsense-account': ['ca-pub-1793586690744453'],
-  },
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-1793586690744453"></meta>
+      </head>
       <body>
         <Navbar />
         {children}
